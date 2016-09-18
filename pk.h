9145f1f8460cceeb214cd49e40e5fd346fca680b
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2016-09-18 10:39:43 +0200

    Better comment

--- pk.h
+++ pk.h
@@ -14,11 +14,13 @@
 #include "macros.h"
 #include "types.h"
 
+// Computes the value of a coalition C (assumes only one driver per coalition)
 // C = Coalition ( C[0] = Cardinality, C[1] ... C[C[0]] = Elements )
 // D = Car or not?
 // SP = Shortest path matrix
 
 #define COALVALUE(C, D, SP) ((D) ? (PATHCOST(minpath((C) + 1, *(C), 1, SP)) + CARCOST) : TICKETCOST)
+
 #define PATHCOST(p) ROUND(penny, (float)(p) / METERSPERLITRE * PENNYPERLITRE)
 #define DIST(dx, dy) (sqrt((dx) * (dx) + (dy) * (dy)))
 #define ROUND(type, i) ((type)(i))
